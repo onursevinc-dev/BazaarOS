f1b41198bdbf726d86d0b0c4378a4c1da294f471
Onur Sevinç <onursevinc.dev@gmail.com>
2025-02-04 00:44:55 +0300

    *

---
+++ src/app/dashboard/admin/layout.tsx
@@ -0,0 +1,24 @@
+import Header from "@/components/dashboard/header/header";
+import Sidebar from "@/components/dashboard/sidebar/sidebar";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import React, { ReactNode } from "react";
+
+export default async function AdminDashboardLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  // Block non admins from accessing the admin dashboard
+  const user = await currentUser();
+  if (!user || user.privateMetadata.role !== "ADMIN") redirect("/");
+  return (
+    <div className="w-full h-full">
+      <Sidebar isAdmin />
+      <div className="ml-[300px]">
+        <Header />
+        <div className="w-full mt-[75px] p-4">{children}</div>
+      </div>
+    </div>
+  );
+}
