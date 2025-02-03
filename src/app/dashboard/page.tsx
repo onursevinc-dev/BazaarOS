2a601d3a02a4bb54449dcd78adbd73305399f778
Onur Sevinç <onursevinc.dev@gmail.com>
2025-02-04 00:45:03 +0300

    *

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  // Get user and redirect depending on the role
+  const user = await currentUser();
+  if (!user?.privateMetadata?.role || user?.privateMetadata.role === "USER")   redirect("/");
+
+  if(user.privateMetadata.role ==="ADMIN") redirect("/dashboard/admin")
+  if(user.privateMetadata.role ==="SELLER") redirect("/dashboard/seller")
+}
