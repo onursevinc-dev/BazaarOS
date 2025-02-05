1a69e8029126dd7f9c1b764eb7a9ae589ea5f5c3
Onur Sevinç <onursevinc.dev@gmail.com>
2025-02-06 01:05:21 +0300

    done category crud

--- src/app/dashboard/admin/categories/page.tsx
+++ src/app/dashboard/admin/categories/page.tsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { getAllCategories } from "@/queries/category";
 
-export default function AdminCategoriesPage() {
-  return <div>admin categories page</div>;
+// Data table
+import DataTable from "@/components/ui/data-table";
+import { Plus } from "lucide-react";
+import CategoryDetails from "@/components/dashboard/forms/category-details";
+import { columns } from "./columns";
+
+export default async function AdminCategoriesPage() {
+  const categories = await getAllCategories();
+
+  // Check if no categories are found
+  if (!categories) return null; //If no categories found, return null
+
+  return (
+    <DataTable
+      actionButtonText={
+        <>
+          <Plus size={15} />
+        </>
+      }
+      modalChildren={<CategoryDetails />}
+      filterValue="name"
+      data={categories}
+      searchPlaceholder="Search category name..."
+      columns={columns}
+    />
+  );
 }
