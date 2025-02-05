e7481bea5a1aa97dc4df0433f70b97c88972b4fc
Onur Sevinç <onursevinc.dev@gmail.com>
2025-02-06 01:05:20 +0300

    done category crud

--- src/app/dashboard/admin/categories/new/page.tsx
+++ src/app/dashboard/admin/categories/new/page.tsx
@@ -2,11 +2,9 @@ import CategoryDetails from "@/components/dashboard/forms/category-details";
 import React from "react";
 
 export default function AdminNewCategoryPage() {
-  const CLOUDINARY_CLOUD_KEY = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_KEY;
-  if (!CLOUDINARY_CLOUD_KEY) return null;
   return (
     <div>
-      <CategoryDetails cloudinary_key={CLOUDINARY_CLOUD_KEY} />
+      <CategoryDetails />
     </div>
   );
 }
