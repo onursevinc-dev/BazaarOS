f58860306f638117a0215b43a7a82c953cc75a41
Onur Sevinç <onursevinc.dev@gmail.com>
2025-02-12 02:07:10 +0300

    product-details componendt developing - ClickToAdd Componennt creating developing

--- src/app/dashboard/admin/categories/page.tsx
+++ src/app/dashboard/admin/categories/page.tsx
@@ -21,6 +21,7 @@ export default async function AdminCategoriesPage() {
         </>
       }
       modalChildren={<CategoryDetails />}
+      newTabLink="/dashboard/admin/categories/new"
       filterValue="name"
       data={categories}
       searchPlaceholder="Search category name..."
