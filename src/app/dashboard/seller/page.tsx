56bf362d4031e7eb08eb3faa9cf5f7556610cca4
Onur Sevinç <onursevinc.dev@gmail.com>
2025-02-12 02:07:12 +0300

    product-details componendt developing - ClickToAdd Componennt creating developing

--- src/app/dashboard/seller/page.tsx
+++ src/app/dashboard/seller/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+// Next.js
+import { redirect } from "next/navigation";
 
-export default function SellerDashboard() {
-  return (
-    <div>
-      Seller dashboard
-    </div>
-  )
+// Clerk
+import { currentUser } from "@clerk/nextjs/server";
+
+// DB
+import { db } from "@/lib/db";
+
+export default async function SellerDashboardPage() {
+  // Fetch the current user. If the user is not authenticated, redirect them to the home page.
+  const user = await currentUser();
+  if (!user) {
+    redirect("/");
+    return; // Ensure no further code is executed after redirect
+  }
+
+  // Retrieve the list of stores associates with the authenticated user.
+  const stores = await db.store.findMany({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  // If the user has no stores, redirect them to the page for creating a new store
+  if (stores.length === 0) {
+    redirect("/dashboard/seller/stores/new");
+    return; // Ensure no further code is executed after redirect
+  }
+
+  // If the user has stores, redirect them to the dashboard of their firts store.
+  redirect(`/dashboard/seller/stores/${stores[0].url}`);
+  // return <div>Stores:{JSON.stringify(stores)}</div>;
 }
