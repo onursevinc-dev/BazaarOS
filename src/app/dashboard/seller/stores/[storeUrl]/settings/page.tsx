fcb1cf3011b528a295e2ba5f8fdf46ff0367b9bb
Onur Sevinç <onursevinc.dev@gmail.com>
2025-02-12 02:09:43 +0300

    product-details componendt developing - ClickToAdd Componennt creating developing

---
+++ src/app/dashboard/seller/stores/[storeUrl]/settings/page.tsx
@@ -0,0 +1,21 @@
+import StoreDetails from "@/components/dashboard/forms/store-details";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import React from "react";
+
+export default async function SellerStoreSettingsPage({
+  params,
+}: {
+  params: { storeUrl: string };
+}) {
+  const storeDetails = await db.store.findUnique({
+    where: { url: params.storeUrl },
+  });
+
+  if (!storeDetails) redirect("/dashboard/seller/stores");
+  return (
+    <div>
+      <StoreDetails data={storeDetails} />
+    </div>
+  );
+}
